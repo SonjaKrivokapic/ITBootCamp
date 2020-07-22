eb97c7cafd1c3672b1b0f7562969fb1c1a38e687
SonjaKrivokapic <skrivokapic88@gmail.com>
2020-07-22 18:59:13 +0200

    adding homework

---
+++ nedelja2/Sreda/zadatak1.js
@@ -0,0 +1,10 @@
+{
+let a = 18;
+let b = 6;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a^b, Math.pow(a,b));
+}
---
+++ nedelja2/Sreda/zadatak2.js
@@ -0,0 +1,16 @@
+
+{
+    let cena = 10;
+    let novcanik = 20;
+    let trenutnoStanje 
+
+    if (cena > novcanik){
+        trenutnoStanje = novcanik - cena
+        console.log("Nemate dovoljno novca. Trenutno stanje:", trenutnoStanje)
+}
+    else if (cena < novcanik){
+        trenutnoStanje = novcanik - cena
+        console.log("Uspesno ste kupili proizvod. Trenutno stanje:", trenutnoStanje)
+}
+}
+ 
