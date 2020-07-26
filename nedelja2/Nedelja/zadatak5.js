55e0aee103ea974d932184bcb96371b64c790c53
SonjaKrivokapic <skrivokapic88@gmail.com>
2020-07-26 19:20:27 +0200

    modified

--- nedelja2/Nedelja/zadatak5.js
+++ nedelja2/Nedelja/zadatak5.js
@@ -1,10 +1,12 @@
 {
-    let n = 5;
-    let taraba = '';
-
-    for(i = 0; i <= n; i++){
-        taraba += ' '.repeat(n-i) + '#'.repeat(i) + ' ' + '#'.repeat(i);
-        taraba += '\n';
+    let n = 5
+    
+    for (i = 0; i <= n; i++){
+        let red = ' '.repeat(n - i) + '#'.repeat(i) + ' ' + '#'.repeat(i) + ' '.repeat(n - i)
+        
+        console.log(red)
     }
-    console.log(taraba);
+
 }
+
+
