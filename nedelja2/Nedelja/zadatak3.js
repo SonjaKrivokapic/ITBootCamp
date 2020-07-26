e6ae19df826eb91d8726c1d001fa634bf4602f40
SonjaKrivokapic <skrivokapic88@gmail.com>
2020-07-26 17:36:32 +0200

    adding homework

--- nedelja2/Nedelja/zadatak3.js
+++ nedelja2/Nedelja/zadatak3.js
@@ -1,14 +1,3 @@
-{
-    let n = 5;
-    let taraba = '';
-
-    for(i = 0; i <= n; i++){
-        taraba += '#'.repeat(i);
-        taraba += '\n';
-    }
-    console.log(taraba);
-}
-
 let n = 5
 
 let red = '#'.repeat + ' '.repeat
