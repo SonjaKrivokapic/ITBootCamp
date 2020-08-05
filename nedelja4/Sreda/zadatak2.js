961d8d26f07515e1e973136ad7d3d94344c3abfc
SonjaKrivokapic <skrivokapic88@gmail.com>
2020-08-05 23:54:29 +0200

    adding homework

---
+++ nedelja4/Sreda/zadatak2.js
@@ -0,0 +1,38 @@
+    let pokemon1 = {
+        ime: 'Bulbsaur',
+        vrsta: 'Grass',
+        Sposobnosti: ['Aroma Veil', 'Blaze, Baterry', 'Air Lock'],
+        Karakteristike: {napad: 45, odbrana: 93, brzina: 40}
+    }
+    let  pokemon2 = {
+        ime: 'Nidoran',
+        vrsta: 'poison',
+        Sposobnosti: ['Poison Point','Hustle','Rivalry'],
+        Karakteristike: {napad: 58, odbrana: 30, brzina: 70}
+    }
+    let pokemon3 = {
+        ime: 'Sandslash',
+        vrsta: 'Ground',
+        Sposobnosti: ['Sand veil', 'Sand Rush', 'Snow Cloack'],
+        Karakteristike: {napad: 20, odbrana:50 , brzina: 100}
+    }
+    let pokemon4 = {
+        ime: 'Raichu',
+        vrsta: 'Electric',
+        Sposobnosti: ['Static', 'Surge Surfer', 'Lightning Rod'],
+        Karakteristike: {napad: 60, odbrana: 90, brzina: 50}
+    }
+
+    let pokemoniNiz = ['pokemon1','pokemon2','pokemon3','pokemon4']
+
+    console.log(pokemoniNiz)
+
+    function nizObjekata(pokemoniNiz){
+        let  nizSposobnosti = [];
+        for(i = 0; i < pokemoniNiz.length; i++){
+            nizSposobnosti.push(pokemoniNiz[i].Sposobnosti);
+            console.log(nizSposobnosti);
+        }
+        return nizSposobnosti
+
+    }
