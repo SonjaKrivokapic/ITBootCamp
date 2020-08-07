aac9bd472e1a1b8b50e69036f659e6fddda926bd
SonjaKrivokapic <skrivokapic88@gmail.com>
2020-08-07 08:19:14 +0200

    adding homework

---
+++ nedelja4/Sreda/zadatak3.js
@@ -0,0 +1,5 @@
+{
+sortedBySpeed = [Bulbsaur.karakteristike.brzina, Nidoran.karakteristike.brzina, Sandslash.karakteristike.brzina, Raichu.karakteristike.brzina];
+    sortedBySpeed.sort((a, b) => a - b);
+    console.log(sortedBySpeed);
+}   
